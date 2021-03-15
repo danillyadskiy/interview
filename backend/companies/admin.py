38018c4b871f705d81e44f158d88589376fc433f
danillyadskiy <danillyadskiy@gmail.com>
2021-03-15 14:58:00 +0300

    2.i 1. The list of companies displays fields pk, name, update_time 2. Search by company name available

--- backend/companies/admin.py
+++ backend/companies/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Okopf, Okved, Region, Company
 
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'update_time')
+    search_fields = ['name']
+
+
 admin.site.register(Okopf)
 admin.site.register(Okved)
 admin.site.register(Region)
-admin.site.register(Company)
+admin.site.register(Company, CompanyAdmin)
