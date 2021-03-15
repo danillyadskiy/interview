4ec81f912744a178da6f9a0ed16edb8b04ee9bbe
danillyadskiy <danillyadskiy@gmail.com>
2021-03-15 14:53:56 +0300

    2.i All models of the project are connected (Okopf, Okved, Region, Company)

--- backend/companies/admin.py
+++ backend/companies/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Okopf, Okved, Region, Company
+
+admin.site.register(Okopf)
+admin.site.register(Okved)
+admin.site.register(Region)
+admin.site.register(Company)
