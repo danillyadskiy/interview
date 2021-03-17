2205515cc6f174da13f3bff107187e361eb6d7c9
danillyadskiy <danillyadskiy@gmail.com>
2021-03-17 12:38:04 +0300

    3 1. Company card added 2. Phrase "Nothing found!" added 3. Empty fields are not displayed (v-if is used only on optional fields) 4. Added check for the presence of companies for a keyword 5. btn-search class for magnifier icon added

--- frontend/src/pages/CompaniesPage.vue
+++ frontend/src/pages/CompaniesPage.vue
@@ -2,17 +2,106 @@
   <v-container class="mt-5" >
       <v-row no-gutters>
         <v-col cols="12" sm="1" md="1">
-          <v-btn text icon v-on:click="searchCompany">
+          <v-btn
+              class="btn-search"
+              text
+              icon
+              v-on:click="searchCompany">
             <v-icon>mdi-magnify</v-icon>
           </v-btn>
         </v-col>
         <v-col cols="12" sm="10" md="10">
+
+          <!-- Строка поиска -->
           <v-text-field
               outlined
               placeholder="Введите название компании или региона"
               v-model="keyWordSearch"
               v-on:keyup.enter="searchCompany"
           ></v-text-field>
+
+          <!-- Карточка компании -->
+          <v-expansion-panels multiple v-if="companies">
+            <v-expansion-panel
+                v-for="company in companies"
+                :key="company.name"
+            >
+              <v-expansion-panel-header>
+                <v-row>
+                  <v-col cols="4">{{ company.name }}</v-col>
+                  <v-col cols="8">{{ company.region }}</v-col>
+                </v-row>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-divider></v-divider>
+                <v-row class="mt-4" no-gutters >
+                  <v-col cols="4">
+                    <v-row align="center">
+                      <v-col cols="3">ИНН</v-col>
+                      <v-col cols="8">{{ company.inn }}</v-col>
+                    </v-row>
+                    <v-row align="center">
+                      <v-col cols="3">КПП</v-col>
+                      <v-col cols="8">{{ company.kpp }}</v-col>
+                    </v-row>
+                    <v-row align="center" v-if="company.registration_date">
+                      <v-col cols="3">дата регистрации</v-col>
+                      <v-col cols="8">{{ company.registration_date }}</v-col>
+                    </v-row>
+                    <v-row align="center">
+                      <v-col cols="3">ОКОПФ</v-col>
+                      <v-col cols="8">{{ company.okopf }}</v-col>
+                    </v-row>
+                    <v-row align="center" no-gutters v-if="company.email">
+                      <v-col cols="3">email</v-col>
+                      <v-col cols="8">
+                        <v-btn icon :href="'mailto:' + company.email">
+                          <v-icon>mdi-email</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                    <v-row align="center" no-gutters v-if="company.link">
+                      <v-col cols="3">сайт</v-col>
+                      <v-col cols="8">
+                        <v-btn
+                            icon
+                            :href="company.link"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                          <v-icon>mdi-link</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="8" v-if="company.main_okved">
+                    <v-row align="center">
+                      <v-col cols="1">ОКВЭД</v-col>
+                    </v-row>
+                    <v-row align="center" no-gutters>
+                      <v-col cols="1">{{ company.main_okved }}</v-col>
+                      <v-col cols="11">{{ company.main_okved_name }}</v-col>
+                    </v-row>
+                    <v-row
+                        align="center"
+                        no-gutters
+                        class="okved"
+                        v-for="okved in company.okved"
+                        :key="okved.code"
+                    >
+                      <v-col cols="1">{{ okved.code }}</v-col>
+                      <v-col cols="11">{{ okved.name }}</v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+
+          <!-- Результат поиска отсутствует -->
+          <div class="text-center" v-if="isCompaniesEmpty">
+            Ничего не найдено!
+          </div>
+
         </v-col>
         <v-col cols="12" sm="1" md="1">
         </v-col>
@@ -31,6 +120,7 @@ export default {
   data() {
     return {
       companies: [],
+      isCompaniesEmpty: false,
       keyWordSearch: this.$route.query.q || ''
     }
   },
@@ -40,9 +130,14 @@ export default {
   methods: {
     searchCompany: function () {
       if (this.keyWordSearch) {
-        this.$router.replace({ query: {q: this.keyWordSearch} })
+        // Замена параметра в строке запроса в соотвествии с ключевым словом поиска
+        this.$router.replace({ query: {q: this.keyWordSearch} }).catch(() => {});
+        // Запрос к API, запись и проверка на наличие результатов
         api.get({q: this.keyWordSearch})
-            .then(response => {this.companies = response.data;});
+            .then(response => {
+              this.companies = response.data;
+              this.isCompaniesEmpty = !response.data.length;
+            });
       }
     }
   }
@@ -50,9 +145,12 @@ export default {
 </script>
 
 <style scoped>
-.v-btn {
+.btn-search {
   float: right;
   margin-top: 10px;
   margin-right: 5px;
+}
+.okved {
+  color: #9e9e9e;
 }
 </style>
